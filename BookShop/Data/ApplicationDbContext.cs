29c74e33ab8dd090c11fad6b8f50cd562c984bb7
HabibIbrahimov <habibyi@code.edu.az>
2022-03-07 18:39:07 +0400

    feat:created category model

---
+++ BookShop/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BookShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShop.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
