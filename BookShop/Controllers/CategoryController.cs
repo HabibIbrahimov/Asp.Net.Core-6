23c7fb34ff07dcaa8e25fd766451b802a3b2c7b4
HabibIbrahimov <habibyi@code.edu.az>
2022-03-09 15:40:12 +0400

    feat:added notification partialview

--- BookShop/Controllers/CategoryController.cs
+++ BookShop/Controllers/CategoryController.cs
@@ -36,6 +36,7 @@ namespace BookShop.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
             return View(obj);
@@ -73,6 +74,7 @@ namespace BookShop.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");
             }
             return View(obj);
@@ -109,6 +111,7 @@ namespace BookShop.Controllers
 
             _db.Categories.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");
 
         }
